6601a6d35d123f4c28b36d19de56257c3928ab6b
chris-code <chris-code@users.noreply.github.com>
2015-07-06 19:37:20 +0200

    Moved geometric shapes to a seperate file
    Points, Lines and Trapezoid classes are now in their own file: shapes.py

--- trapezoid_decomposition.py
+++ trapezoid_decomposition.py
@@ -1,76 +1,6 @@
 import random
+from shapes import *
 import visualization as vis
-
-indent_string = '  '
-
-class Point:
-	class_counter = 0
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
-		
-		self.ident = Point.class_counter
-		Point.class_counter += 1
-	
-	def is_left_of(self, point):
-		return self.x < point.x
-		
-	def is_right_of(self, point):
-		return self.x > point.x
-	
-	def is_above(self, line):
-		# Sarrus scheme
-		det = line.q.x * self.y + line.p.x * line.q.y + line.p.y * self.x - line.p.y * line.q.x - line.q.y * self.x - self.y * line.p.x
-		return det > 0
-	
-	def __str__(self, indent=0):
-		string_representation = indent_string * indent
-		string_representation += 'Po{0}: ({1},{2})'.format(self.ident, self.x, self.y)
-		return string_representation
-
-class Line:
-	class_counter = 0
-	def __init__(self, p, q):
-		self.p = p
-		self.q = q
-		self.ident = Line.class_counter
-		Line.class_counter += 1
-	
-	def __str__(self, indent=0):
-		string_representation = indent_string * indent
-		string_representation += 'Li{0}: ({1},{2}->{3},{4})'.format(self.ident, self.p.x, self.p.y, self.q.x, self.q.y)
-		return string_representation
-
-class Trapezoid:
-	class_counter = 0
-	def __init__(self, top, bot, leftp, rightp):
-		self.top = top
-		self.bot = bot
-		self.leftp = leftp
-		self.rightp = rightp
-		
-		self.nw = None
-		self.ne = None
-		self.sw = None
-		self.se = None
-		
-		self.ident = Trapezoid.class_counter
-		Trapezoid.class_counter += 1
-	
-	def __str__(self, indent=0):
-		string_representation = indent_string * indent
-		string_representation += 'Tr{0}: Top={1}, Bot={2}, LeftP={3}, RightP={4}'
-		string_representation += 'nw={5}, ne={6}, sw={7}, se={8}'
-		nw, ne, sw, se = None, None, None, None
-		if self.nw is not None:
-			nw = self.nw.ident
-		if self.ne is not None:
-			ne = self.ne.ident
-		if self.sw is not None:
-			sw = self.sw.ident
-		if self.se is not None:
-			se = self.se.ident
-		return string_representation.format(self.ident, self.top, self.bot, self.leftp, self.rightp, nw, ne, sw, se)
 
 class Decomposition(set):
 	def get_intersected_trapezoids(self, D, line):
@@ -117,10 +47,8 @@ class Tree:
 		else:
 			raise Exception('Nope.')
 	
-	def __str__(self, indent=0):
-		pass #TODO
-		string_representation = indent_string * indent
-		string_representation += '{0}'.format(self.content)
+	def __str__(self, indent=0, indent_string = '  '):
+		string_representation = self.content.__str__(indent)
 		if not isinstance(self.content, Trapezoid):
 			string_representation += '\n'
 			string_representation += self.left.__str__(indent+1)
